cb7f9e310ef626cad75308114e9c3caef43255aa
browlm13 <browlm13@gmail.com>
2017-10-01 04:44:18 -0500

    Update summary.py

--- summary.py
+++ summary.py
@@ -8,6 +8,7 @@
 import math
 import json
 import os
+import logging
 
 # mylib
 from rake_sentence_ranking import *
@@ -22,14 +23,19 @@ from nltk.corpus import stopwords
 from nltk.tag import pos_tag
 from nltk.corpus import wordnet
 
+# logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 #
 #	Data Set Methods
 #
 
 def load_scraped_subreddit_document_set(path, subreddit, titles=False):
 	""" loads all json files from dataset matching subreddit format: "subreddit_fname.extension" and returns list of ['title','text'] """
-	files = [f for f in os.listdir(path) if f.split('_')[0] == subreddit]
+	logger.info('Loading Document Set')
 
+	files = [f for f in os.listdir(path) if f.split('_')[0] == subreddit]
 	documents = []
 	for f in files:
 		with open(path + f) as data_file:    
@@ -40,6 +46,7 @@ def load_scraped_subreddit_document_set(path, subreddit, titles=False):
 		else:
 			documents.append(data['text'])
 
+	logger.info('Document Set Loading Complete')
 	return documents
 
 #
@@ -96,12 +103,16 @@ def similarity_score(term, tokenized_document):
 
 def processes_and_tokenize(raw_document):
 	""" remove punctuation, convert to lower case, and return list of tokens """
+	logger.info('Cleaning Text')
+
 	tokenizer = RegexpTokenizer(r'\w+')
 	tokens = tokenizer.tokenize(raw_document.lower())		# tokens = nltk.word_tokenize(corpus.lower()) # without removing punctiation
 
 	#remove stop words
 	stop_words = set(stopwords.words('english'))
 	filtered_tokens = [w for w in tokens if not w in stop_words]
+
+	logger.info('Cleaning Text Complete')
 	return filtered_tokens
 
 #
@@ -110,11 +121,17 @@ def processes_and_tokenize(raw_document):
 
 def word_frequency_dict(tokens):
 	""" returns a dictionary of word and their assosiated frequencies from token list """
+	logger.info('Building Word Frequency Dictionary')
 
 	fdist = FreqDist(tokens) 						# fdist.keys() fdist.values()
+	logger.info('Word Frequency Dictionary Completed')
+
 	return dict(fdist)
 
 def term_fequency(term,tokens):
+	""" Return term frequency / number of terms in token list """
+	logger.info('Calculating Term Frequency')
+
 	term = processes_and_tokenize(term)[0]	#make sure term is in correct form
 
 	tf = tokens.count(term)
@@ -122,6 +139,8 @@ def term_fequency(term,tokens):
 
 def augmented_term_fequency(term,tokens):
 	""" returns term frequency in tokens over maximum term frequency of tokens """
+	logger.info('Calculating Augmented Term Frequency')
+
 	term = processes_and_tokenize(term)[0] #make sure term is in correct form
 
 	max_count = max([tokens.count(t) for t in tokens])
@@ -129,6 +148,8 @@ def augmented_term_fequency(term,tokens):
 
 def inverse_document_frequency(term, tokenized_documents_list):
 	""" IDF(t) = ln( Number Of Documents / Number Of Documents Containg Term )."""
+	logger.info('Calculating IDF')
+
 	term = processes_and_tokenize(term)[0]	#make sure term is in correct form
 
 	num_documents = len(tokenized_documents_list)
@@ -140,6 +161,8 @@ def inverse_document_frequency(term, tokenized_documents_list):
 
 def nolog_inverse_document_frequency(term, tokenized_documents_list):
 	""" IDF(t) = ln( Number Of Documents / Number Of Documents Containg Term )."""
+	logger.info('Calculating no-log IDF')
+
 	term = processes_and_tokenize(term)[0]	#make sure term is in correct form
 
 	num_documents = len(tokenized_documents_list)
@@ -150,6 +173,8 @@ def nolog_inverse_document_frequency(term, tokenized_documents_list):
 
 def tf_idf(term, tokenized_document, tokenized_documents_list):
 	""" Term Frequency - Inverse Document Frequency : returns tf * idf """
+	logger.info('Calculating TF-IDF')
+
 	#return term_fequency(term, tokenized_document) * inverse_document_frequency(term, tokenized_documents_list)
 	#return augmented_term_fequency(term, tokenized_document) * inverse_document_frequency(term, tokenized_documents_list)
 	return term_fequency(term, tokenized_document) * nolog_inverse_document_frequency(term, tokenized_documents_list)
@@ -159,6 +184,8 @@ def tf_idf(term, tokenized_document, tokenized_documents_list):
 #
 
 def keyword_score(term, tokenized_document, tokenized_documents_list):
+	logger.info('Calculating Keyword Score')
+
 	tf_idf_scaler = 2
 	term_tf_idf_score = tf_idf(term,tokenized_document,tokenized_documents_list)
 	term_similarity_score = similarity_score(term, tokenized_document)
@@ -166,6 +193,8 @@ def keyword_score(term, tokenized_document, tokenized_documents_list):
 
 
 def keyword_scores_for_part_of_speech(pos, tokenized_document, tokenized_documents_list):
+	logger.info('Calculating Keyword Score For Part Of Speech')
+
 	tagged_tokenized_document = pos_tag(tokenized_document)
 	filtered_tokens = [term for term, tag in tagged_tokenized_document if tag == pos]
 	return list(filtered_tokens)
